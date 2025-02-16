857459d1367346f342d31ee0231e983494c4f0d9
shaishabcoding <shaishabchandrashil@gmail.com>
2025-02-16 09:40:57 +0600

    feat: update .gitignore and package.json; add new dependencies and improve code formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,49 +1,52 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import Chat from "./pages/chats/Chat";
-import Signup from "./pages/signup/Signup";
-import ChatBox from "./components/chat/ChatBox";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "./redux/store";
-import Login from "./pages/login/Login";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import Chat from './pages/chats/Chat';
+import Signup from './pages/signup/Signup';
+import ChatBox from './components/chat/ChatBox';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
+import Login from './pages/login/Login';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "chat",
-        element: <Chat />,
-        children: [
-          {
-            path: ":id",
-            element: <ChatBox />,
-          },
-        ],
-      },
-      {
-        path: "signup",
-        element: <Signup />,
-      },
-      {
-        path: "login",
-        element: <Login />,
-      },
-    ],
-  },
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				path: 'chat',
+				element: <Chat />,
+				children: [
+					{
+						path: ':id',
+						element: <ChatBox />,
+					},
+				],
+			},
+			{
+				path: 'signup',
+				element: <Signup />,
+			},
+			{
+				path: 'login',
+				element: <Login />,
+			},
+		],
+	},
 ]);
 
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Provider {...{ store }}>
-      <PersistGate loading="loading auth" persistor={persistor}>
-        <RouterProvider router={router} />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<Provider {...{ store }}>
+			<PersistGate
+				loading="loading auth"
+				persistor={persistor}
+			>
+				<RouterProvider router={router} />
+			</PersistGate>
+		</Provider>
+	</React.StrictMode>
 );
